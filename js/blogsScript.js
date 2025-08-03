2362faf605cc08bb12b234537fb8c1bcf9467d4c
balbir <prasadbalbir1056@gmail.com>
2025-08-03 12:40:22 +0530

    Fix #27: Add "Read More" link to blog previews

--- js/blogsScript.js
+++ js/blogsScript.js
@@ -1,35 +1,43 @@
-import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js'
-import app from './firebaseSetup.js'
-const firestore = getFirestore(app)
-const blogs = document.querySelector('#blogs')
-const loadingIndicator = document.querySelector('#loadingContainer')
+import {
+  getFirestore,
+  collection,
+  getDocs,
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import app from "./firebaseSetup.js";
+const firestore = getFirestore(app);
+const blogs = document.querySelector("#blogs");
+const loadingIndicator = document.querySelector("#loadingContainer");
 
+// Function to truncate text beyond 50 chars and add "Read More" link
+function truncateText(text, maxLength = 50) {
+  if (text.length <= maxLength) {
+    return text;
+  }
+  return (
+    text.substring(0, maxLength) +
+    '... <span class="read-more">Read More</span>'
+  );
+}
 
-async function getBlogs () {
-		const querySnapshot = await getDocs(collection(firestore, "blogsRef"))
-		 querySnapshot.forEach((doc) => {
-			const docData = doc.data()
-			blogs.innerHTML += `<a class="blog" id="${doc.id}" href="/viewBlog?blogId=${docData.blogId}"><img class="blogImage" src="./assets/placeholderImage.jpg"><h3>${docData.title}</h3><p>${docData.subline}</p></a>`
-		
-		
-
-	  
-		 })
-	hideLoadingIndicator()
-	console.log('hi')
-
-
-
+async function getBlogs() {
+  const querySnapshot = await getDocs(collection(firestore, "blogsRef"));
+  querySnapshot.forEach((doc) => {
+    const docData = doc.data();
+    const truncatedSubline = truncateText(docData.subline);
+    blogs.innerHTML += `<a class="blog" id="${doc.id}" href="/viewBlog?blogId=${docData.blogId}"><img class="blogImage" src="./assets/placeholderImage.jpg"><h3>${docData.title}</h3><p>${truncatedSubline}</p></a>`;
+  });
+  hideLoadingIndicator();
+  console.log("hi");
 }
 
 try {
-	getBlogs()
-	hideLoadingIndicator()
-} catch(error) {
-	console.log(error)
+  getBlogs();
+  hideLoadingIndicator();
+} catch (error) {
+  console.log(error);
 }
 function hideLoadingIndicator() {
-	loadingIndicator.style.display = 'none'
-	console.log('l')
-	loadingIndicator.style.visibility = 'hidden'
+  loadingIndicator.style.display = "none";
+  console.log("l");
+  loadingIndicator.style.visibility = "hidden";
 }
